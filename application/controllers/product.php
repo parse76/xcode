9f88cb7c730d1533c39becb4ffb6983e14a0774c
Bryan Estrito <bryan.estrito@gmail.com>
2013-02-17 23:17:59 +0800

    Adding products controller/model

---
+++ application/controllers/product.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Product's Controller
+*/
+class Product extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	
+}
+
+/* End of file product.php */
+/* Location: ./application/controllers/product.php */
